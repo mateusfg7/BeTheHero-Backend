d7281eec1ea21e547b17db2b94443618f08f7118
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-23 17:10:41 -0300

    initial codes

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Mateus Felipe'
+    })
+});
+
 app.listen(3333);
