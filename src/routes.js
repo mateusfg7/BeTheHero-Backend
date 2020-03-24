e5e3404e6741aa6a7a6394590bed6d4d2b145e58
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-24 15:12:56 -0300

    create incidents controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
 const OngController = require('./controllers/OngController')
+const IncidentsController = require('./controllers/IncidentsController')
 
 const routes = express.Router()
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
+
+routes.get('/incidents', IncidentsController.index)
+routes.post('/incidents', IncidentsController.create)
 
 module.exports = routes
