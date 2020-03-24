da196b01cc1b4a7e72aa50bdd85b7ddcc35cc8c6
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-24 12:44:47 -0300

    create src folder and move index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+app.post('/users/', (request, response) => {
+    const body = request.body
+
+    console.log(body)
+    
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Mateus Felipe Gonçalves'
+    })
+});
+
+app.listen(3333);
