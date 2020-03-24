de574a403d4a0dda2a52bb53601707519b60152d
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-24 14:39:24 -0300

    create ong list

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,12 @@ const connection = require('./database/connection')
 
 const routes = express.Router()
 
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*')
+
+    return response.json(ongs)
+})
+
 routes.post('/ongs', async (request, response) => {
     const { name, email, whatsapp, city, uf } = request.body
 
@@ -19,6 +25,6 @@ routes.post('/ongs', async (request, response) => {
     })
 
     return response.json({ id })
-});
+})
 
 module.exports = routes
