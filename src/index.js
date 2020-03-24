451bb593f19366394d74ec223494680373c8ab64
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-24 12:49:22 -0300

    create routes file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 const express = require('express');
+const routes = require('./routes')
 
 const app = express();
 
 app.use(express.json())
-
-app.post('/users/', (request, response) => {
-    const body = request.body
-
-    console.log(body)
-    
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Mateus Felipe Gonçalves'
-    })
-});
+app.use(routes)
 
 app.listen(3333);
