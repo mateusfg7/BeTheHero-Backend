368e40e1ab44faacbeb1988a32d5f6fee40bccbb
mateusfg7 <mateusfelipefg77@gmail.com>
2020-03-24 15:25:41 -0300

    create delete incident method

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,6 @@ routes.post('/ongs', OngController.create)
 
 routes.get('/incidents', IncidentsController.index)
 routes.post('/incidents', IncidentsController.create)
+routes.delete('/incidents/:id', IncidentsController.delete)
 
 module.exports = routes
